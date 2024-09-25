38ef03230ecadb03111f2bb0c10ea55eb05cfae5
sujan447 <mirzasujan890@gmail.com>
2024-09-25 12:03:02 +0600

    final submition

--- js/index.js
+++ js/index.js
@@ -1,12 +1,11 @@
 
 // 1st donation section------>
-
 document.getElementById('btn-donate-N').addEventListener('click', function(){
     const addMoneyN = getInputFieldValueById('add-money-N');
         const receivedMoneyN = getTextFieldById('received-money-N');
          const mainBalance =getTextFieldById('main-balance');
         
-          if (isNaN(addMoneyN) || addMoneyN > mainBalance ||     addMoneyN < 0) {          
+          if (isNaN(addMoneyN) || addMoneyN > mainBalance ||     addMoneyN <= 0) {          
             alert('Failed to donation');
             document.getElementById('my_modal_1').close()         
             return;
@@ -22,12 +21,10 @@ document.getElementById('btn-donate-N').addEventListener('click', function(){
     div.classList.add('border-2', 'shadow-xl', 'p-6');
    div.innerHTML = `
         <h4 class = "text-xl font-bold">${addMoneyN} Taka is Donated for Flood at Noakhali, Bangladesh </h4> <br>
-        <p class = "shadow-lg shadow-slate-300">Date: ${new Date().toLocaleDateString()}  </p>
+        <p class = "shadow-lg shadow-slate-300 p-2 font-semibold">Date: ${new Date().toString()}  </p>
     `;
     const historyContainer = document.getElementById('donation-list') ;
-    historyContainer.appendChild(div);
-     
-   
+    historyContainer.appendChild(div);  
 })  
 
 
@@ -36,7 +33,7 @@ document.getElementById('btn-donate-F').addEventListener('click', function(){
     const addMoneyF = getInputFieldValueById('add-money-F');
     const receivedMoneyF = getTextFieldById('received-money-F');
     const mainBalance =getTextFieldById('main-balance');
-    if (isNaN(addMoneyF) || addMoneyF > mainBalance ||     addMoneyF < 0) {
+    if (isNaN(addMoneyF) || addMoneyF > mainBalance ||     addMoneyF <= 0) {
         
         alert('Failed to donation');
         document.getElementById('my_modal_2').close();
@@ -49,16 +46,14 @@ document.getElementById('received-money-F').innerText = newBalanceF;
 const remainingBalance = mainBalance - addMoneyF;
 document.getElementById('main-balance').innerText = remainingBalance; 
    
-// const div = document.createElement('div');
-// div.classList.add('border-2', 'shadow-xl', 'p-6');
-// div.innerHTML = `
-//     <h4 class = "text-xl font-bold">${addMoneyN} Taka is Donated for Flood at Noakhali, Bangladesh </h4> <br>
-//     <p class = "shadow-lg shadow-slate-300">Date: ${new Date().toLocaleDateString()}  </p>
-// `;
-// const historyContainer = document.getElementById('donation-listF') ;
-// historyContainer.appendChild(div);
- 
-
+const div = document.createElement('div');
+div.classList.add('border-2', 'shadow-xl', 'p-6');
+div.innerHTML = `
+    <h4 class = "text-xl font-bold">${addMoneyF} Taka is Donated for Flood at Feni, Bangladesh </h4> <br>
+    <p class = "shadow-lg shadow-slate-300 p-2 font-semibold">Date: ${new Date().toString()}  </p>
+`;
+const historyContainer = document.getElementById('donation-list') 
+historyContainer.appendChild(div);
 })
 
 
@@ -67,7 +62,7 @@ document.getElementById('btn-donate-Q').addEventListener('click', function(){
     const addMoneyQ = getInputFieldValueById('add-money-Q');
     const receivedMoneyQ = getTextFieldById('received-money-Q');
     const mainBalance =getTextFieldById('main-balance');
-    if (isNaN(addMoneyQ) || addMoneyQ > mainBalance ||     addMoneyQ < 0) {
+    if (isNaN(addMoneyQ) || addMoneyQ > mainBalance ||     addMoneyQ <= 0) {
         
         alert('Failed to donation');
         document.getElementById('my_modal_3').close();
@@ -76,23 +71,26 @@ document.getElementById('btn-donate-Q').addEventListener('click', function(){
 const newBalanceQ = addMoneyQ + receivedMoneyQ;
 document.getElementById('received-money-Q').innerText = newBalanceQ;
 
-
 const remainingBalance = mainBalance - addMoneyQ;
 document.getElementById('main-balance').innerText = remainingBalance; 
-
+const div = document.createElement('div');
+div.classList.add('border-2', 'shadow-xl', 'p-6');
+div.innerHTML = `
+    <h4 class = "text-xl font-bold">${addMoneyQ} Taka is Donated for Aid for Injured in the Quota Movement </h4> <br>
+    <p class = "shadow-lg shadow-slate-300 p-2 font-semibold">Date: ${new Date().toString()}  </p>
+`;
+const historyContainer = document.getElementById('donation-list') 
+historyContainer.appendChild(div);
 })
 
 // btn features----->
-
 donationBtn = document.getElementById('donation');
 historyBtn = document.getElementById('history');
 historyBtn.addEventListener('click', function(){
     
     historyBtn.classList.add('btn-info', 'text-white');
     donationBtn.classList.remove('btn-info', 'text-white');
-    showFeaturesById('donation-history');
-     
-   
+    showFeaturesById('donation-history');  
 })
 
 donationBtn.addEventListener('click', function(){
